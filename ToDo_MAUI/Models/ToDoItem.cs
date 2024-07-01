235ed5a50d6c89fa22cc7cbbfd7aa0a0bb673b14
BAAARON2 <BAAARON2@365.openu.ac.il>
2024-07-01 20:03:49 +0300

    Add ToDoItem to Models

---
+++ ToDo_MAUI/Models/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDo_MAUI.Models;
+
+internal class ToDoItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool Completed { get; set; }
+    public DateTime Due { get; set; }
+}
