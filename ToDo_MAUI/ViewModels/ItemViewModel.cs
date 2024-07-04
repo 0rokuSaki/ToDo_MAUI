ba6a588dff5f106bfa48351f941b5163cc144e8e
BAAARON2 <BAAARON2@365.openu.ac.il>
2024-07-04 19:53:23 +0300

    Add binding in ItemView

--- ToDo_MAUI/ViewModels/ItemViewModel.cs
+++ ToDo_MAUI/ViewModels/ItemViewModel.cs
@@ -1,13 +1,30 @@
-﻿using ToDo_MAUI.Repositories;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ToDo_MAUI.Models;
+using ToDo_MAUI.Repositories;
 
 namespace ToDo_MAUI.ViewModels;
 
-public class ItemViewModel : ViewModel
+public partial class ItemViewModel : ViewModel
 {
     private readonly IToDoItemRepository _repository;
+
+    [ObservableProperty]
+    ToDoItem _item;
 
     public ItemViewModel(IToDoItemRepository repository)
     {
         _repository = repository;
+        this.Item = new ToDoItem()
+        {
+            Due = DateTime.Now.AddDays(1)
+        };
+    }
+
+    [RelayCommand]
+    public async Task SaveAsync()
+    {
+        await _repository.AddOrUpdateAsync(this.Item);
+        await this.Navigation.PopAsync();
     }
 }
