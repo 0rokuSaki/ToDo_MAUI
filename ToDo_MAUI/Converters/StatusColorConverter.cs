c8f20f994df6f5f241f2fb2fcb634533f56ec626
BAAARON2 <BAAARON2@365.openu.ac.il>
2024-07-04 20:18:57 +0300

    Add color converter

---
+++ ToDo_MAUI/Converters/StatusColorConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace ToDo_MAUI.Converters;
+
+public class StatusColorConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        (Color)Application.Current.Resources[(bool)value ? "CompletedColor" : "ActiveColor"];
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
