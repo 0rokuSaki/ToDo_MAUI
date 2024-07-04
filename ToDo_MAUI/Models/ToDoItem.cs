ebfdf5085776d406bcc367cbed7e68e8d99e314d
BAAARON2 <BAAARON2@365.openu.ac.il>
2024-07-04 23:39:04 +0300

    Add styling

--- ToDo_MAUI/Models/ToDoItem.cs
+++ ToDo_MAUI/Models/ToDoItem.cs
@@ -6,7 +6,7 @@ public class ToDoItem
 {
     [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
-    public string Title { get; set; }
+    public string? Title { get; set; }
     public bool Completed { get; set; }
     public DateTime Due { get; set; }
 }
